31c4b4e032b94dfde098c6cfb2a4356a38c96dec
vincenzomercadante <vincenzo.mercadante0@outlook.it>
2024-05-02 17:01:11 +0200

    feat: add Apartment resource wth seeder & migration

---
+++ database/migrations/2024_05_02_142845_create_apartments_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->id();
+            $table->text('name');
+            $table->string('slug');
+            $table->unsignedTinyInteger('n_room');
+            $table->unsignedTinyInteger('n_bathroom');
+            $table->unsignedTinyInteger('n_bed');
+            $table->unsignedSmallInteger('square_meters');
+            $table->unsignedTinyInteger('floor');
+            $table->string('address');
+            $table->decimal('lat', 10,8);
+            $table->decimal('lon', 11,8);
+            $table->string('cover_img')->nullable();
+            $table->boolean('visible')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartments');
+    }
+};
