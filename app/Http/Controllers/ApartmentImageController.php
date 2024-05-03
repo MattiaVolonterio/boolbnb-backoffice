396a73de607ae1a56fa1c8ccb65dc3b242fc4807
lucapolzone <lucapolzone@gmail.com>
2024-05-03 12:08:05 +0200

    feat: add apartment_images starting resource

---
+++ app/Http/Controllers/ApartmentImageController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Apartment_image;
+use Illuminate\Http\Request;
+
+class ApartmentImageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Apartment_image  $apartment_image
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Apartment_image $apartment_image)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Apartment_image  $apartment_image
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Apartment_image $apartment_image)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Apartment_image  $apartment_image
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Apartment_image $apartment_image)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Apartment_image  $apartment_image
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Apartment_image $apartment_image)
+    {
+        //
+    }
+}
