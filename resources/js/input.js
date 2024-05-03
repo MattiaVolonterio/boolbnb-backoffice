8710cc73854fa950ab383a26dad035af19974113
MattiaVolonterio <volonteriomattia@gmail.com>
2024-05-03 21:19:46 +0200

    feat: prototype of address search

--- resources/js/input.js
+++ resources/js/input.js
@@ -1,37 +1,39 @@
+// const { default: axios } = require("axios");
+
 // get address
-const searchInput = document.getElementById('address');
+const searchInput = document.getElementById("address");
 
 // get latitude
-const lat = document.getElementById('lat');
+const lat = document.getElementById("lat");
 
 // get longitude
-const lon = document.getElementById('lon');
+const lon = document.getElementById("lon");
 
-const form = document.getElementById('form');
+const form = document.getElementById("form");
 
 // get search button
-const searchBtn = document.getElementById('search_btn');
+const searchBtn = document.getElementById("search_btn");
 
-const apiKey = 'cXFRhnBAXKnWWIK6455uRtxFdwAGvyV2';
+const suggestion = document.getElementById("suggestion");
+
+const apiKey = "cXFRhnBAXKnWWIK6455uRtxFdwAGvyV2";
 
 // axios call
-function fetchAddress (){
-    // tomtom api
-    axios.get(`https://api.tomtom.com/search/2/geocode/${searchInput.value}.json?typeahead=true&limit=3&countrySet=IT&lat=37.337&lon=-121.89&view=Unified&key=${apiKey}`)
-        .then(response => {
-        console.log(response.data);
-    })
+function fetchAddress() {
+    suggestion.innerHTML = "";
+    axios
+        .get(
+            `https://api.tomtom.com/search/2/geocode/${searchInput.value}.json?typeahead=true&limit=3&countrySet=IT&lat=37.337&lon=-121.89&view=Unified&key=${apiKey}`
+        )
+        .then((response) => {
+            const results = response.data.results;
+            results.forEach((result) => {
+                const address = result.address.freeformAddress;
+                suggestion.innerHTML += `<div>${address}</div>`;
+            });
+        });
+}
 
-
-    // console.log(`https://api.tomtom.com/search/2/geocode/${searchInput.value}.json?typeahead=true&limit=3&countrySet=IT&lat=37.337&lon=-121.89&view=Unified&key=${apiKey}`)
-
-};
-
-searchBtn.addEventListener('click', () => {
-
-    console.log('ciao');
-
-    fetchAddress ();
+searchBtn.addEventListener("click", () => {
+    fetchAddress();
 });
-
-
--- resources/views/admin/apartments/create.blade.php
+++ resources/views/admin/apartments/create.blade.php
@@ -2,74 +2,70 @@
 
 @section('content')
     <form action="" method="POST" class="container" id="form">
-    @csrf
+        @csrf
 
-    <label for="name" class="form-label">Nome</label>
-    <input type="text" class="form-control" id="name" name="name" />
+        <label for="name" class="form-label">Nome</label>
+        <input type="text" class="form-control" id="name" name="name" />
 
-    <label for="number" class="form-label">N°</label>
-    <input type="text" class="form-control" id="number" name="number" />
+        <label for="number" class="form-label">N°</label>
+        <input type="text" class="form-control" id="number" name="number" />
 
-    <label for="type" class="form-label">Tipo</label>
-    <select class="form-select" id="type" name="type">
-        <option value="lunga">Lunga</option>
-        <option value="corta">Corta</option>
-        <option value="cortissima">Cortissima</option>
-    </select>
+        <label for="type" class="form-label">Tipo</label>
+        <select class="form-select" id="type" name="type">
+            <option value="lunga">Lunga</option>
+            <option value="corta">Corta</option>
+            <option value="cortissima">Cortissima</option>
+        </select>
 
-    <label for="weight" class="form-label">Peso (g)</label>
-    <input type="text" class="form-control" id="weight" name="weight" />
+        <label for="weight" class="form-label">Peso (g)</label>
+        <input type="text" class="form-control" id="weight" name="weight" />
 
-    <label for="img" class="form-label">img</label>
-    <input type="text" class="form-control" id="img" name="img" />
+        <label for="img" class="form-label">img</label>
+        <input type="text" class="form-control" id="img" name="img" />
 
-    <label for="description" class="form-label">Descrizione</label>
-    <textarea
-        class="form-control"
-        id="description"
-        name="description"
-        rows="4"
-    ></textarea>
+        <label for="description" class="form-label">Descrizione</label>
+        <textarea class="form-control" id="description" name="description" rows="4"></textarea>
 
-    {{-- ricerca indirizzo --}}
-    <div class="row">
+        {{-- ricerca indirizzo --}}
+        <div class="row">
 
-        {{-- indirizzo --}}
-        <div class="col-10">
-            <label for="address" class="form-label">Indirizzo</label>
-            <input type="text" class="form-control" id="address" name="address" />
+            {{-- indirizzo --}}
+            <div class="col-10">
+                <div class="search-container">
+                    <label for="address" class="form-label">Indirizzo</label>
+                    <input type="text" class="form-control" id="address" name="address" />
+                    <div id="suggestion"></div>
+                </div>
+            </div>
+
+            <div class="col-2 d-flex">
+                <div id="search_btn" class="btn btn-primary align-self-end w-100">Cerca</div>
+            </div>
+
+            {{-- latitude e longitude --}}
+            <div class="col-6 my-3">
+                <input type="hidden" id="lat" name="lat" value="">
+                <span id="latitude">Lat</span>
+            </div>
+
+            <div class="col-6 my-3">
+                <input type="hidden" id="lon" name="lon" value="">
+                <span id="longitude">Lon</span>
+            </div>
+
         </div>
 
-        <div class="col-2 d-flex">
-            <div id="search_btn" class="btn btn-primary align-self-end w-100">Cerca</div>
-        </div>
 
-        {{-- latitude e longitude --}}
-        <div class="col-6">
-            <input type="hidden" id="lat" name="lat" value="">
-            <span id="latitude">Lat</span>
-        </div>
-
-        <div class="col-6">
-            <input type="hidden" id="lon" name="lon" value="">
-            <span id="longitude">Lon</span>
-        </div>
-
-    </div>
-    
-
-    <button type="submit" class="btn btn-primary mt-2">Salva</button>
-</form>
+        <button type="submit" class="btn btn-primary mt-2">Salva</button>
+    </form>
 @endsection
 
 @section('css')
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
-         integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
-         crossorigin="anonymous" referrerpolicy="no-referrer" />
+        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 @endsection
 
 @section('js')
-
     @vite('resources/js/input.js')
-
 @endsection
