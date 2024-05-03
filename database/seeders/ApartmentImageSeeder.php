eb8b59090a0b759353ad0c14fb9361f57098cf63
lucapolzone <lucapolzone@gmail.com>
2024-05-03 12:50:31 +0200

    feat: add apartment_images.csv & add ApartmentImageSeeder

--- database/seeders/ApartmentImageSeeder.php
+++ database/seeders/ApartmentImageSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Apartment_image;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,28 @@ class ApartmentImageSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // apre il file csv
+        $file = fopen(__DIR__ . "/../csv/apartment_images.csv", "r");
+
+        // variabile di controllo della prima riga del csv
+        $is_first_line = true;
+
+        while (!feof($file)) {
+            $apartment_image_data = fgetcsv($file);
+            if (!$is_first_line) {
+                // nuovo Apartment image
+                $apartment_image = new Apartment_image;
+                $apartment_image->url = $apartment_image_data[1];
+                // salvataggio nel db
+                $apartment_image->save();
+            }
+
+            // set variabile di controllo su false dopo prima iterazione
+            $is_first_line = false;
+        }
+
+
+
+
     }
 }
