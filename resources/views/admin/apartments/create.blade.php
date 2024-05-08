ef913e665ffbf6d7b5cdaadff19bd222e8bfceb5
vincenzomercadante <vincenzo.mercadante0@outlook.it>
2024-05-08 10:56:27 +0200

    fix: view create apartment validation

--- resources/views/admin/apartments/create.blade.php
+++ resources/views/admin/apartments/create.blade.php
@@ -5,16 +5,17 @@
         @csrf   </form> --}}
 
     <div class="container ">
-        <form class="was-validated mt-5" action="{{ route('admin.apartments.store', $apartment) }}" method="POST"
+
+        <form class="mt-5" action="{{ route('admin.apartments.store', $apartment) }}" method="POST"
             enctype="multipart/form-data">
             @csrf
-
+            <h1 class="mb-3">Creazione Appartamento</h1>
             <div class="row g-5">
                 <div class="col-6">
                     {{-- nome del appartamento  --}}
                     <div class="form-floating mb-4">
-                        <input type="text" class="form-control" id="name" name="name"
-                            value="{{ old('name', $apartment->name) }}" required>
+                        <input type="text" class="form-control @error('name') is-invalid @enderror" id="name"
+                            name="name" value="{{ old('name') }}" required>
                         <label for="name" class="form-label">Inserisci Nome del Appartamento<span class="text-danger"> *
                             </span></label>
                         @error('name')
@@ -26,8 +27,9 @@
                         {{--  nr stanze  --}}
                         <div class="col-4">
                             <div class="form-floating">
-                                <input type="number" min="1" class="form-control" id="n_room" name="n_room"
-                                    value="{{ old('n_room', $apartment->n_room) }}" required>
+                                <input type="number" min="1"
+                                    class="form-control @error('n_room') is-invalid @enderror" id="n_room" name="n_room"
+                                    value="{{ old('n_room') }}" required>
                                 <label for="n_room" class="form-label">Inserisci nr stanze<span class="text-danger"> *
                                     </span></label>
                                 @error('n_room')
@@ -38,8 +40,9 @@
                         <div class="col-4">
                             {{-- nr bagni --}}
                             <div class="form-floating">
-                                <input type="number" min="1" class="form-control" id="n_bathroom" name="n_bathroom"
-                                    value="{{ old('n_bathroom', $apartment->n_bathroom) }}" required>
+                                <input type="number" min="1"
+                                    class="form-control @error('n_bathroom') is-invalid @enderror" id="n_bathroom"
+                                    name="n_bathroom" value="{{ old('n_bathroom') }}" required>
                                 <label for="n_bathroom" class="form-label">Inserisci nr bagni<span class="text-danger"> *
                                     </span></label>
                                 @error('n_bathroom')
@@ -50,8 +53,9 @@
                         <div class="col-4">
                             {{-- nr letti --}}
                             <div class="form-floating">
-                                <input type="number" min="1" class="form-control" id="n_bed" name="n_bed"
-                                    value="{{ old('n_bed', $apartment->n_bed) }}" required>
+                                <input type="number" min="1"
+                                    class="form-control @error('n_bed') is-invalid @enderror" id="n_bed" name="n_bed"
+                                    value="{{ old('n_bed') }}" required>
                                 <label for="n_bed" class="form-label">Inserisci nr letti<span class="text-danger"> *
                                     </span></label>
                                 @error('n_bed')
@@ -65,8 +69,9 @@
                         {{-- nr piano --}}
                         <div class="col-4">
                             <div class="form-floating">
-                                <input type="number" min="1"class="form-control" id="floor" name="floor"
-                                    value="{{ old('floor', $apartment->floor) }}" required>
+                                <input type="number"
+                                    min="1"class="form-control @error('floor') is-invalid @enderror" id="floor"
+                                    name="floor" value="{{ old('floor') }}" required>
                                 <label for="floor" class="form-label">Inserisci nr piano<span class="text-danger"> *
                                     </span></label>
                                 @error('floor')
@@ -77,10 +82,10 @@
                         {{-- nr superficie --}}
                         <div class="col-4">
                             <div class="form-floating">
-                                <input type="number" min="1" max="255" class="form-control" id="square_meters"
-                                    name="square_meters" value="{{ old('square_meters', $apartment->square_meters) }}"
-                                    required>
-                                <label for="square_meters" class="form-label">superfice<span class="text-danger"> *
+                                <input type="number" min="1" max="255"
+                                    class="form-control @error('square_meters') is-invalid @enderror" id="square_meters"
+                                    name="square_meters" value="{{ old('square_meters') }}" required>
+                                <label for="square_meters" class="form-label">superfice (mq) <span class="text-danger"> *
                                     </span></label>
                                 @error('square_meters')
                                     <div class="invalid-feedback">{{ $message }}</div>
@@ -98,71 +103,40 @@
                                 {{-- indirizzo --}}
                                 <div class="col-12 mb-3 z-1">
                                     <div class="search-container form-floating">
-                                        <input type="text" class="form-control" id="address" name="address"
-                                            value="{{ old('address', $apartment->address) }}" required>
+                                        <input type="text" class="form-control @error('address') is-invalid @enderror"
+                                            id="address" name="address" value="{{ old('address') }}" required>
                                         <label for="address" class="form-label">Indirizzo</label>
                                         <div id="suggestion"></div>
                                     </div>
                                 </div>
 
-                                {{--  <div class="col-2 d-flex">
-                                        <div id="search_btn" class="btn btn-primary align-self-end w-100">Cerca</div>
-                                    </div> --}}
-
-
-
-
                                 {{-- latitude e longitude  solo per debug --}}
-                                <div class="col-6 my-3 z-0">
-                                    <div class="form-floating">
-                                        <input class="form-control" id="lat" name="lat"
-                                            value="{{ old('lat', $apartment->lat) }}">
-                                        <label for="lat" id="latitude">Lat</label>
-                                    </div>
-
+                                <div class="col-6 d-none">
+                                    <input type="hidden" id="lat" name="lat" value="{{ old('lat') }}">
+                                    <input type="hidden" id="lon" name="lon" value="{{ old('lon') }}">
                                 </div>
-
-                                <div class="col-6 my-3 z-0">
-                                    <div class="form-floating">
-                                        <input class="form-control" id="lon" name="lon"
-                                            value="{{ old('lon', $apartment->lon) }}">
-                                        <label for="lon" id="longitude">Lon</label>
-                                    </div>
-                                </div>
-
                             </div>
                         </div>
                         {{-- Upload Foto --}}
                         <div class="col mb-3">
                             <label for="cover_img" class="form-label mb-1">Carica la cover</label>
-                            <input class="form-control" type="file" name="cover_img" id="cover_img">
+                            <input class="form-control @error('cover_img') is-invalid @enderror" type="file"
+                                name="cover_img" id="cover_img">
                         </div>
                         <div class="mb-3">
                             <label for="apartment_images" class="form-label mb-1">*Carica altre foto</label>
-                            <input class="form-control" type="file" id="apartment_images" name="apartment_images[]"
-                                multiple>
+                            <input class="form-control @error('apartment_images') is-invalid @enderror" type="file"
+                                id="apartment_images" name="apartment_images[]" multiple>
                         </div>
 
                         {{-- servizi --}}
                         <div class="row ">
-                            <h3 class="text-center text-primary">Servizi</h3>
-                            @foreach ($services as $service)
-                                <div class="col-md-4">
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" value="{{ $service->id }}"
-                                            id="service_{{ $service->id }}" name="services[]"
-                                            {{ $apartment->services->contains($service->id) ? 'checked' : '' }}>
-                                        <label class="form-check-label text-primary" for="service_{{ $service->id }}">
-                                            {{ $service->name }}
-                                        </label>
-                                    </div>
-                                </div>
-                            @endforeach
+
                         </div>
                         {{-- Switch --}}
                         <div class="form-check form-switch m-3">
-                            <input class="form-check-input" type="checkbox" role="switch" id="visible"
-                                name="visible">
+                            <input class="form-check-input @error('visible') is-invalid @enderror" type="checkbox"
+                                role="switch" id="visible" name="visible" value="on">
                             <label class="form-check-label" for="visible">Visibile</label>
                         </div>
 
@@ -173,12 +147,22 @@
 
                 </div>
 
-                <div class="col-6 ">
-                    <div class="card mx-auto w-75">
-                        @if (!empty($apartment->cover_img))
-                            <img src="{{ asset('storage/' . $apartment->cover_img) }}" class="card-img-top img-fluid"
-                                style="width: 100%; height: auto;" alt="apartment cover img" style="width: 200px; ">
-                        @endif
+                <div class="col-6">
+                    <h3 class="text-center text-primary mb-3">Aggiungi Servizi</h3>
+                    <div class="row">
+                        @foreach ($services as $service)
+                            <div class="col-md-4">
+                                <div class="form-check">
+                                    <input class="form-check-input @error('services') is-invalid @enderror"
+                                        type="checkbox" value="{{ $service->id }}" id="service_{{ $service->id }}"
+                                        name="services[]"
+                                        {{ $apartment->services->contains($service->id) ? 'checked' : '' }}>
+                                    <label class="form-check-label" for="service_{{ $service->id }}">
+                                        {{ $service->name }}
+                                    </label>
+                                </div>
+                            </div>
+                        @endforeach
                     </div>
                 </div>
 
