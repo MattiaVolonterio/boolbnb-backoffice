047236f6c6e3267fb8735564a24e9d75b81d3aed
lucapolzone <lucapolzone@gmail.com>
2024-05-08 16:09:29 +0200

    fix: details

--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -50,12 +50,12 @@
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="deleteMessageLabel{{ $message->id }}">Eliminare
+                        <h5 class="modal-title" id="deleteMessageLabel{{ $message->id }}">Elimina il messaggio di
                             {{ $message->name }}</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                        Premendo elimina l'azione sarà irreversibile. Procedere?
+                        Premendo "Elimina" l'azione sarà irreversibile. Procedere?
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Chiudi</button>
