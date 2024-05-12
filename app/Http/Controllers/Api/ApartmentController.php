473a3edcb1d16c967069e704aa206b05567d45e8
MattiaVolonterio <volonteriomattia@gmail.com>
2024-05-12 19:52:38 +0200

    fix

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -173,7 +173,7 @@ class ApartmentController extends Controller
             foreach ($apartments_filtered as $apartment) {
                 $apartment->cover_img = $apartment->cover_img ? asset('storage/' . $apartment->cover_img) : 'https://placehold.co/600x400';
             }
-            $filtered_apartments_paginated = $this->paginate($apartments_filtered::toArray());
+            $filtered_apartments_paginated = $this->paginate($apartments_filtered);
             return response()->json($filtered_apartments_paginated);
         } else {
             foreach ($apartments as $apartment) {
