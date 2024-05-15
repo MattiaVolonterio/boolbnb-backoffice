66d8ffacc11f17fa64185745c1e3339fbb25da6f
MattiaVolonterio <volonteriomattia@gmail.com>
2024-05-15 11:50:05 +0200

    feat: comments in message and visit model

--- app/Models/Visit.php
+++ app/Models/Visit.php
@@ -15,5 +15,6 @@ class Visit extends Model
         return $this->belongsTo(Apartment::class);
     }
 
+    // Riga da decommentare per fare il seeder, poi ricommentare
     // public $timestamps = false;
 }
