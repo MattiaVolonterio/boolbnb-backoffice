1f434dac96131a0cfa689644439cc549cae00e79
lucapolzone <lucapolzone@gmail.com>
2024-05-16 11:53:08 +0200

    feat: add validation control, name cannot be a number

--- app/Http/Requests/MessageStoreRequest.php
+++ app/Http/Requests/MessageStoreRequest.php
@@ -26,22 +26,39 @@ class MessageStoreRequest extends FormRequest
     public function messages()
     {
         return[
-            'apartment_id.required' => 'Appartmaneto non valido',
-            'apartment_id.integer' => 'Appartmaneto non valido',
-            'apartment_id.exists' => 'Appartmaneto non valido',
+            'apartment_id.required' => 'Appartamento non valido',
+            'apartment_id.integer' => 'Appartamento non valido',
+            'apartment_id.exists' => 'Appartamento non valido',
 
-            'name.required' => 'Nome non valido',
-            'name.string' => 'Nome non valido',
-            'name.max' => 'Nome non valido',
+            'name.required' => 'Questo campo non deve essere vuoto',
+            'name.string' => 'Inserisci un nome',
+            'name.max' => 'Hai superato il limite massimo di caratteri',
 
-            'content.required' => 'Messaggio non valido',
-            'content.max' => 'Messaggio non valido',
-            'content.string' => 'Messaggio non valido',
+            'content.required' => 'Questo campo non deve essere vuoto',
+            'content.max' => 'Il tuo messaggio è troppo lungo',
+            'content.string' => 'Inserisci del testo',
 
-            'customer_email.required' => 'Email non valida',
-            'customer_email.max' => 'Email non valida',
-            'customer_email.email' => 'Email non valida'
+            'customer_email.required' => 'Questo campo non deve essere vuoto',
+            'customer_email.max' => 'Inserisci un indirizzo email valido',
+            'customer_email.email' => 'Inserisci un indirizzo email valido'
         ];
+    }
+
+    protected function withValidator($validator)
+    {
+        $validator->after(function ($validator) {
+            // Se è un numero, aggiungo un errore di validazione
+            if ($this->nameIsNumeric($this->input('name'))) {
+                $validator->errors()->add('name', 'Il nome non può essere un numero.');
+            }
+        });
+    }
+
+    protected function nameIsNumeric($name)
+    {
+        // Controllo se è un numero
+        return is_numeric($name);
+
     }
 
     public function failedValidation(Validator $validator){
